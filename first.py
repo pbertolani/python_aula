39f8cbd13930e74f130d92780a34b79eaf461028
pbertolani <pedrobertolani22@gmail.com>
2026-01-08 22:21:28 -0300

    fifth commit

--- first.py
+++ first.py
@@ -10,3 +10,18 @@ limite_saque_diario = 1000
 BRAZILIAN_STATES = ["SP, RJ, SC, PR, RS"]
 
 print(BRAZILIAN_STATES)
+
+#Conversão de tipos
+
+print(int(1.9))
+print(int("10"))
+print(float("10.10"))
+print(float(100))
+
+valor = 10
+valor_str = str(valor)
+print(type(valor))
+print(type(valor_str))
+
+print(100 / 2)
+print(100 // 2)
