e7183b525ed6b4d41b114d3454c9fcd465abaecb
pbertolani <pedrobertolani22@gmail.com>
2026-01-20 22:55:44 -0300

    fifth commit

--- operadores.py
+++ operadores.py
@@ -32,3 +32,15 @@ print(produto_1 ** produto_2)
 print(produto_1 / produto_2)
 print(produto_1 // produto_2)
 print(produto_1 % produto_2)
+
+x = 10 + 5 * 4
+print(x)
+
+y = (10 + 5) * 4
+print(y)
+
+z = 10 / 2 + 25 + 2 - 100 ** 2
+print(z)
+
+a = (10 / 2) + (25 + 2) - (100 ** 2)
+print(a)
