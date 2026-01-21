3bb5134a04d187580e5fe1e1826ae685258ba570
pbertolani <pedrobertolani22@gmail.com>
2026-01-20 22:31:30 -0300

    forth commit

--- operadores.py
+++ operadores.py
@@ -11,3 +11,23 @@ print(12 // 4)
 print( 10 % 3)
 
 print(2 ** 3)
+
+print(10 - 5 *2)
+
+print((10 - 5) * 2)
+
+print(10 ** 2 * 2)
+
+print(10 ** (2 * 2))
+
+print(10 / 2 * 4)
+
+produto_1 = 10
+produto_2 = 20
+
+print(produto_1 + produto_2)
+print(produto_1 - produto_2)
+print(produto_1 * produto_2)
+print(produto_1 ** produto_2)
+print(produto_1 / produto_2)
+print(produto_1 // produto_2)
