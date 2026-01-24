a1bb0352d4b77e3134b7af8503fad4bf5a010841
pbertolani <pedrobertolani22@gmail.com>
2026-01-24 18:39:23 -0300

    second commit

--- estrutura_condicional_aninhada.py
+++ estrutura_condicional_aninhada.py
@@ -10,6 +10,8 @@ if conta_normal:
         print("Saque realizado com sucesso!")
     elif saque <= (saldo + cheque_especial):
         print("Saque realizado com uso do cheque especial!")
+    else:
+        print("Não foi possível realizar o saque, saldo insuficiente!")
 elif conta_universitaria:
     if saldo >= saque:
         print("Saque realizado com sucesso!")
