d9069c2445e92cd569ee779974fc79540aa46a03
pbertolani <pedrobertolani22@gmail.com>
2026-01-24 17:38:07 -0300

    first commit

---
+++ identacao_blocos.py
@@ -0,0 +1,10 @@
+def sacar(valor: float):
+    saldo = 500
+
+    if saldo >= valor:
+        print("valor sacado!")
+        print("retire seu dinheiro no caixa.")
+
+    print("Obrigado por ser nosso cliente, tenha um bom dia!")
+
+sacar(100)
