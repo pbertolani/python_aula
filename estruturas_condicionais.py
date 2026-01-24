64959d03d93bb63d1953b797e11c3e1c96a9df61
pbertolani <pedrobertolani22@gmail.com>
2026-01-24 18:21:58 -0300

    second commit

--- estruturas_condicionais.py
+++ estruturas_condicionais.py
@@ -1,4 +1,5 @@
 MAIOR_IDADE = 18
+IDADE_ESPECIAL = 12
 
 idade = int(input("Informe a sua idade: "))
 
@@ -10,5 +11,12 @@ if idade < MAIOR_IDADE:
 
 if idade >= MAIOR_IDADE:
     print("Maior de idade, pode tirar a CNH.")
+else:
+    print("Ainda não pode tirar a CNH.")
+
+if idade >= MAIOR_IDADE:
+    print("Maior de idade, pode tirar a CNH.")
+elif idade == IDADE_ESPECIAL:
+    print("Pode fazer as aulas teóricas, mas não pode fazer aulas práticas.")
 else:
     print("Ainda não pode tirar a CNH.")
