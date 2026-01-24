ed9bc4a71b80fc0f51d3dfeca0accc21ab84d81e
pbertolani <pedrobertolani22@gmail.com>
2026-01-23 21:16:33 -0300

    nineth commit

--- operadores.py
+++ operadores.py
@@ -62,14 +62,29 @@ print(saldo <= saque)
 
 #atribuição
 
-saldo = 500
+saldo = 200
 print(saldo)
 
 saldo = 200
 print(saldo)
 
-saldo += 200
-saldo -= 200
-saldo /= 200
-saldo //= 200
-saldo *= 200
+saldo += 10
+print(saldo)
+
+saldo -= 5
+print(saldo)
+
+saldo /= 2
+print(saldo)
+
+saldo //= 2
+print(saldo)
+
+saldo *= 10
+print(saldo)
+
+saldo %= 3
+print(saldo)
+
+saldo **= 2
+print(saldo)
